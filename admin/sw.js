7f132ca23b5cae4fbe8b29fa0cf5b5398faff59b
KingTrap42 <95733298+KingTrap42@users.noreply.github.com>
2025-02-04 22:53:25 +0700

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -1,44 +1,57 @@
-const CACHE_NAME = "cache_v1";
-const URLS_TO_CACHE = [
-    '/',
-    '/styles/main.css',
-    '/script/main.js',
-    '/images/logo.png'
+const cacheName = "v1";
+const preCache = [
+    "/",
+    "/index.php",
+    "../assets/css/sb-admin-2.min.css",
+    "../assets/vendor/fontawesome-free/css/all.min.css",
+    "../assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    "../assets/vendor/jquery/jquery.min.js"
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(URLS_TO_CACHE);
-            })
+self.addEventListener("install", (e) => {
+    console.log("Service worker installed");
+
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            await cache.addAll(preCache);
+        })()
     );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", event => {
+    console.log("Service worker ready");
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
+                cacheNames.filter(cacheName => cacheName !== 'v1').map(cacheName => caches.delete(cacheName))
             );
         })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            const resCache = await cache.match(e.request);
+
+            if (resCache) {
+                return resCache;
+            }
+
+            try {
+                const res = await fetch(e.request);
+
+                if (res && res.ok) {
+                    const clonedRes = res.clone();
+                    cache.put(e.request, clonedRes);
+                }
+
+                return res;
+            } catch (error) {
+                console.log(error);
+            }
+        })()
     );
 });
